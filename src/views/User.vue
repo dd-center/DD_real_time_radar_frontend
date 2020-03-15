ff7143dcca3bcc767679f305b768a41840408d15
ScilientHill <pren1@ucsc.edu>
2020-03-15 15:47:35 -0700

    upgrade2

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="container">
     <p>TODO: API 合并完成✅；需要有返回房间列表的接口</p>
+    <Status-combination class="level-item has-text-centered"></Status-combination>
     <line-chart/>
     <pie-chart/>
-<!--    <Rank-value class="level-item has-text-centered"></Rank-value>-->
-<!--    <Danmaku-value class="level-item has-text-centered"></Danmaku-value>-->
-<!--    <Working-value class="level-item has-text-centered"></Working-value>-->
-    <Status-combination class="level-item has-text-centered"></Status-combination>
   </div>
 </template>
 
 <script>
 
+const StatusCombination = () => import("../components/status_combination")
 const LineChart = () => import("../components/Linechart")
 const PieChart = () => import("../components/Piechart")
-// const RankValue = () => import("../components/rank")
-// const DanmakuValue = () => import("../components/total_danmaku")
-// const WorkingValue = () => import("../components/is_working")
-const StatusCombination = () => import("../components/status_combination")
 
 export default {
   components: {
+    StatusCombination,
     LineChart,
     PieChart,
-    // RankValue,
-    // DanmakuValue,
-    // WorkingValue,
-    StatusCombination,
   },
 }
 
