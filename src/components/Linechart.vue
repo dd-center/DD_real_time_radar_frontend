7e0a47418257606904d187f2f1107e2fecfbb1e1
ScilientHill <pren1@ucsc.edu>
2020-03-23 18:55:55 -0700

    save

--- src/components/Linechart.vue
+++ src/components/Linechart.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="card">
     <h2 class="title">{{ title }}</h2>
-    <chart ref="chart1" :options="options" :auto-resize="true"></chart>
     <b-tabs content-class="mt-3">
       <!-- Render Tabs, supply a unique `key` to each tab -->
       <b-tab v-for="i in Object.keys(data)" :key="'dyn-tab-' + i" :title="i" v-on:click="onChange">
-        Tab contents {{ i }}
       </b-tab>
     </b-tabs>
+    <chart ref="chart1" :options="options" :auto-resize="true"></chart>
   </div>
 </template>
 
@@ -42,33 +41,8 @@ export default {
       // console.log(value)
       this.value = value
       // console.log(this.$refs.b1)
-      this.keys = Object.keys(this.value)
-      const key = this.keys[this.index % this.keys.length]
-      this.title = key + ' 同传弹幕统计'
-
-      this.options = {
-        xAxis: {
-          type: 'category',
-          data: this.value[key].x_axis,
-        },
-        yAxis: {
-          type: 'value',
-        },
-        tooltip: {
-          trigger: 'axis',
-          formatter(data) {
-            var colorSpan = color => '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + color + '"></span>';
-            let rez = '<p>' + data[0].axisValue + '</p>';
-            data.forEach((data_value) => {
-              if (!data_value.value == "") {
-                rez += '<p>'   + colorSpan(data_value.color) + ' ' + data_value.seriesName + ': ' + data_value.value + '</p>'
-              }
-            });
-            return rez
-          }
-        },
-        series: this.value[key].data,
-      }
+      this.keys = Object.keys(this.value);
+      this.update_chart()
       // index += 1
     },
   },
@@ -76,7 +50,12 @@ export default {
       onChange(value) {
         const target_value = value.target.innerText;
         this.index = this.keys.indexOf(target_value);
-        console.log(this.value)
+        // console.log(this.value)
+        this.update_chart()
+        // console.log(this.keys.indexOf(target_value));
+      },
+
+      update_chart() {
         const key = this.keys[this.index % this.keys.length]
         this.title = key + ' 同传弹幕统计'
         this.options = {
@@ -102,10 +81,6 @@ export default {
           },
           series: this.value[key].data,
         }
-
-
-
-        // console.log(this.keys.indexOf(target_value));
       }
     }
 }
