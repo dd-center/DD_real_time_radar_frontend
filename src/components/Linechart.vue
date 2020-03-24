6f66d67d9b3f59da9a37585632d1e9486e6b5989
ScilientHill <pren1@ucsc.edu>
2020-03-23 18:36:32 -0700

    K!

--- src/components/Linechart.vue
+++ src/components/Linechart.vue
@@ -3,8 +3,10 @@
     <h2 class="title">{{ title }}</h2>
     <chart ref="chart1" :options="options" :auto-resize="true"></chart>
     <b-tabs content-class="mt-3">
-      <b-tab title="First" v-on:click="onChange"><p>I'm the first tab</p></b-tab>
-      <b-tab title="Second" v-on:click="onChange"><p>I'm the second tab</p></b-tab>
+      <!-- Render Tabs, supply a unique `key` to each tab -->
+      <b-tab v-for="i in Object.keys(data)" :key="'dyn-tab-' + i" :title="i" v-on:click="onChange">
+        Tab contents {{ i }}
+      </b-tab>
     </b-tabs>
   </div>
 </template>
@@ -17,7 +19,7 @@ import 'echarts/lib/chart/bar'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-let index = 0
+// let index = 0
 
 export default {
   mixins: [mixin],
@@ -25,6 +27,9 @@ export default {
   data: () => ({
     title: '',
     options: {},
+    keys: [],
+    index: 0,
+    value: []
   }),
 
   created() {
@@ -35,15 +40,16 @@ export default {
   watch: {
     data(value) {
       // console.log(value)
-      console.log(this.$refs.b1)
-      const keys = Object.keys(value)
-      const key = keys[index % keys.length]
+      this.value = value
+      // console.log(this.$refs.b1)
+      this.keys = Object.keys(this.value)
+      const key = this.keys[this.index % this.keys.length]
       this.title = key + ' 同传弹幕统计'
 
       this.options = {
         xAxis: {
           type: 'category',
-          data: value[key].x_axis,
+          data: this.value[key].x_axis,
         },
         yAxis: {
           type: 'value',
@@ -61,14 +67,45 @@ export default {
             return rez
           }
         },
-        series: value[key].data,
+        series: this.value[key].data,
       }
-      index += 1
+      // index += 1
     },
   },
   methods: {
       onChange(value) {
-         console.log(value.target.innerText);
+        const target_value = value.target.innerText;
+        this.index = this.keys.indexOf(target_value);
+        console.log(this.value)
+        const key = this.keys[this.index % this.keys.length]
+        this.title = key + ' 同传弹幕统计'
+        this.options = {
+          xAxis: {
+            type: 'category',
+            data: this.value[key].x_axis,
+          },
+          yAxis: {
+            type: 'value',
+          },
+          tooltip: {
+            trigger: 'axis',
+            formatter(data) {
+              var colorSpan = color => '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + color + '"></span>';
+              let rez = '<p>' + data[0].axisValue + '</p>';
+              data.forEach((data_value) => {
+                if (!data_value.value == "") {
+                  rez += '<p>'   + colorSpan(data_value.color) + ' ' + data_value.seriesName + ': ' + data_value.value + '</p>'
+                }
+              });
+              return rez
+            }
+          },
+          series: this.value[key].data,
+        }
+
+
+
+        // console.log(this.keys.indexOf(target_value));
       }
     }
 }
